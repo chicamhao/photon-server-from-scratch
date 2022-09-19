e6bb0c0a3d3ffa102b08572906a554b9f1f9126f
chicamhao <bluevampire0103@gmail.com>
2022-09-19 16:42:52 +0700

    + create first plugin

---
+++ src-server/Plugins/MyFirstPlugin/MyFirstPlugin.cs
@@ -0,0 +1,29 @@
+﻿using Photon.Hive.Plugin;
+using System;
+using System.Collections.Generic;
+
+namespace MyFirstPlugin
+{
+    public class MyFirstPlugin : PluginBase
+    {
+        public override string Name => "MyFirstPlugin";
+        private IPluginLogger pluginLogger;
+
+        public override bool SetupInstance(IPluginHost host, Dictionary<string, string> config, out string errorMsg)
+        {
+            pluginLogger = host.CreateLogger(Name);
+            return base.SetupInstance(host, config, out errorMsg);
+        }
+
+        public override void OnCreateGame(ICreateGameCallInfo info)
+        {
+            //a log message in a callback
+            pluginLogger.InfoFormat("***************************************************");
+            pluginLogger.InfoFormat("OOOOOOOOOOOOOOOOOOOOOOOOOnCreateGame {0} by user {1}", info.Request.GameId, info.UserId);
+            pluginLogger.InfoFormat("***************************************************");
+
+            info.Continue();
+        }
+
+    }
+}
---
+++ src-server/Plugins/MyFirstPlugin/MyPluginFactory.cs
@@ -0,0 +1,13 @@
+﻿using Photon.Hive.Plugin;
+
+
+namespace MyFirstPlugin
+{
+    class MyPluginFactory : PluginFactoryBase
+    {
+        public override IGamePlugin CreatePlugin(string pluginName)
+        {
+            return new MyFirstPlugin();
+        }
+    }
+}
